aeeea9ed92831f5534f231b0900667c93fa4e86d
jmpark14 <jmbuddy815@gmail.com>
2020-01-23 17:22:07 +0900

    두번째 커밋

--- src/jm/HelloWorld.java
+++ src/jm/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("HelloWorld"); // 이 부분이 바뀌었으니 바뀐 ~.java만 커밋해줌
 
 	}
 
